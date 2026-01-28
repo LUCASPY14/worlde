4ad5cd9b348b331d7d17a121585cda6eaf309162
Lucas Gimenez <lucaspy14@gmail.com>
2026-01-28 15:09:23 -0300

    creamos la variable palabras_secretas = random.choice las palabras cargadas para ser random y la cantidad de intentos que tendra el usuario

--- wordle.py
+++ wordle.py
@@ -1,3 +1,7 @@
 import random
 
 Palabras = ["perro","gatos","campo", "silla", "carta", " nubes", "pluma", "llave","fuego","piano"]
+
+palabras_secretas = random.choice(Palabras)
+intentos_max = 6
+
